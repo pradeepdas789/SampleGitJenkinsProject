a0949574040d4571e37b323c6185bbe63c959154
Pradeep Das <pradeepdas789@gmail.com>
2017-04-27 11:51:33 -0400

    Initial checkin to SampleGitJenkinsProject

---
+++ SampleGitJenkinsProject/src/com/demo/gitproject/sampleMainClass.java
@@ -0,0 +1,11 @@
+package com.demo.gitproject;
+
+public class sampleMainClass {
+
+	public static void main(String[] args) {
+		testClass tc = new testClass();
+		System.out.println("Indie Main Class - Test1");
+		tc.fun();
+	}
+
+}
---
+++ SampleGitJenkinsProject/src/com/demo/gitproject/testClass.java
@@ -0,0 +1,9 @@
+package com.demo.gitproject;
+
+public class testClass {
+	
+	public void fun(){
+	System.out.println("Inside test Class - Test1");
+	}
+	
+}
