d8ca7b050091ad069a0bd8b244cecd17b0529479
Pradeep Das <pradeepdas789@gmail.com>
2017-05-02 11:25:22 -0400

    3rd Change

--- SampleGitJenkinsProject/src/com/demo/gitproject/sampleMainClass.java
+++ SampleGitJenkinsProject/src/com/demo/gitproject/sampleMainClass.java
@@ -6,6 +6,7 @@ public class sampleMainClass {
 		testClass tc = new testClass();
 		System.out.println("Indie Main Class - Test1");
 		System.out.println("Indie Main Class - Test2");
+		System.out.println("Indie Main Class - Test3");
 		tc.fun();
 	}
 
